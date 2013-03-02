ef3290728d15d5739f1021fa28a4c1206c1a8431
Lee Willis <leewillis77@gmail.com>
2013-03-02 19:52:48 +0000

    First take an admin area

--- say-what-admin.php
+++ say-what-admin.php
@@ -15,11 +15,40 @@ class say_what_admin {
 
 	private $settings;
 
-
+	/**
+	 * Constructor
+	 */
 	function __construct($settings) {
 
 		$this->settings = $settings;
+		add_action( 'admin_init', array ( &$this, 'admin_init' ) );
 
+	}
+
+
+
+	function admin_init() {
+
+        if ( current_user_can ( 'manage_options' ) ) {
+
+            $page = add_management_page ( __('Text changes', 'say_what'),
+                                          __('Text changes', 'say_what'),
+                                          'manage_options',
+                                          'say_what_admin',
+                                          array ( $this, 'admin' ) );
+
+            $this->admin_url = 'tools.php?page=say_what_admin'; // FIXME - not needed?
+
+            add_action('admin_print_styles-' . $page, array ( &$this, 'enqueue_scripts' ) );
+
+        }
+
+	}
+
+
+
+	function enqueue_scripts() {
+		// Placeholder - suspect I might want some admin JS later
 	}
 
 }
