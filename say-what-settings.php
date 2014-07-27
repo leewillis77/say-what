7eded4394829896b0cd47a5f7ee6772a7173a623
Lee Willis <leewillis77@gmail.com>
2014-07-27 20:32:24 +0100

    Whitespace / coding standards

--- say-what-settings.php
+++ say-what-settings.php
@@ -1,27 +1,17 @@
 <?php
 
-
 if ( ! defined( 'ABSPATH' ) )
     exit; // Exit if accessed directly
-
-
 
 /**
  * Settings class. Possibly overkill at the moment
  */
 class say_what_settings {
-
 	public $replacements;
-
 	function __construct() {
-
 		global $wpdb, $table_prefix;
-
 		// @TODO - Read other settings in when we have them
 		$sql = "SELECT * FROM {$table_prefix}say_what_strings";
-
-		$this->replacements = $wpdb->get_results ( $sql, ARRAY_A );
-
+		$this->replacements = $wpdb->get_results( $sql, ARRAY_A );
 	}
-
 }
