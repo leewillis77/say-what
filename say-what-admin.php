0c75171eeff2b1bced326a2333952f01a3c17ef4
Lee Willis <leewillis77@gmail.com>
2014-07-27 20:40:52 +0100

    Actually translate strings with context

--- say-what-admin.php
+++ say-what-admin.php
@@ -80,6 +80,9 @@ class say_what_admin {
 	 * Render the list of currently configured replacement strings
 	 */
 	public function admin_list() {
+		_e( 'Bart', 'say_what' );
+		_ex( 'Bart', 'Little', 'say_what' );
+		_ex( 'Bart', 'Big', 'say_what' );
 		require_once('html/say_what_admin_list.php');
 	}
 
--- say-what-frontend.php
+++ say-what-frontend.php
@@ -13,21 +13,31 @@ class say_what_frontend {
     /**
      * Read the settings in and put them into a format optimised for the final filter
      */
-	function __construct($settings) {
+	function __construct( $settings ) {
 		foreach ( $settings->replacements as $key => $value ) {
 			if ( empty ( $value['domain'] ) )
-				$value['domain'] = 'default';
-			$this->replacements[$value['domain']][$value['orig_string']] = $value['replacement_string'];
+				$value['domain'] = 'sw-default-domain';
+			if ( empty ( $value['context'] ) )
+				$value['context'] = 'sw-default-context';
+			$this->replacements[$value['domain']][$value['orig_string']][$value['context']] = $value['replacement_string'];
 		}
-		add_filter ( 'gettext', array ( $this, 'gettext' ), 10, 3 );
+		add_filter( 'gettext', array( $this, 'gettext' ), 10, 3 );
+		add_filter( 'gettext', array( $this, 'gettext_with_context' ), 10, 4 );
 	}
 
     /**
-     * The main function - optionally perform a string replacement
+     * Perform a string replacement without context.
      */
-	function gettext( $translated, $original, $domain ) {
-		if ( isset ( $this->replacements[$domain][$original] ) ) {
-			return $this->replacements[$domain][$original];
+	public function gettext( $translated, $original, $domain ) {
+		return $this->gettext_with_context( $translated, $original, 'sw-default-context', $domain );
+	}
+
+	/**
+	 * Perform a string replacement with context.
+	 */
+	public function gettext_with_context( $translated, $original, $context, $domain ) {
+		if ( isset ( $this->replacements[$domain][$original][$context] ) ) {
+			return $this->replacements[$domain][$original][$context];
 		} else {
 			return $translated;
 		}
