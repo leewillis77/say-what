6d725c36cb3d124d26403b5959da9ba803acff5a
Lee Willis <leewillis77@gmail.com>
2014-10-21 20:27:50 +0100

    Minor function definition cleanup

--- say-what-settings.php
+++ say-what-settings.php
@@ -7,8 +7,10 @@ if ( ! defined( 'ABSPATH' ) )
  * Settings class. Possibly overkill at the moment
  */
 class say_what_settings {
+
 	public $replacements;
-	function __construct() {
+
+	public function __construct() {
 		global $wpdb, $table_prefix;
 		// @TODO - Read other settings in when we have them
 		$sql = "SELECT * FROM {$table_prefix}say_what_strings";
