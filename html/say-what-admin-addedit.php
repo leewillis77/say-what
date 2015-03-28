795358ab7939021d4c8243a53dda8985dc311447
Teoh Han Hui <teohhanhui@gmail.com>
2015-03-28 15:24:16 +0800

    Fix instructions getting in the way of add/edit

--- html/say-what-admin-addedit.php
+++ html/say-what-admin-addedit.php
@@ -15,11 +15,11 @@
 			<input type="text" name="say_what_orig_string" size="120" value="<?php echo esc_attr( $replacement->orig_string ) ?>"><br/>
 		</p>
 		<p>
-			<label for="say_what_domain"><?php _e( 'Text domain', 'say_what' ); ?></label> <a href="http://plugins.leewillis.co.uk/doc_post/adding-string-replacement/"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
+			<label for="say_what_domain"><?php _e( 'Text domain', 'say_what' ); ?></label> <a href="http://plugins.leewillis.co.uk/doc_post/adding-string-replacement/" target="_blank" tabindex="-1"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
 			<input type="text" name="say_what_domain" size="30" value="<?php echo esc_attr( $replacement->domain ); ?>"><br/>
 		</p>
 		<p>
-			<label for="say_what_context"><?php _e( 'Text context', 'say_what' ); ?></label> <a href="http://plugins.leewillis.co.uk/doc_post/replacing-wordpress-strings-context/"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
+			<label for="say_what_context"><?php _e( 'Text context', 'say_what' ); ?></label> <a href="http://plugins.leewillis.co.uk/doc_post/replacing-wordpress-strings-context/" target="_blank" tabindex="-1"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
 			<input type="text" name="say_what_context" size="30" value="<?php echo esc_attr( $replacement->context ); ?>"><br/>
 		</p>
 		<p>
