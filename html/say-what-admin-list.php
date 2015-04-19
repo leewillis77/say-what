17089b20969da127a3143000bce1938e0db56d02
Lee Willis <leewillis77@gmail.com>
2015-04-19 21:35:24 +0100

    Add info box about Pro version

--- html/say-what-admin-list.php
+++ html/say-what-admin-list.php
@@ -6,4 +6,10 @@
 		$list_table_instance->prepare_items();
 		$list_table_instance->display();
 	?>
+	<div class="saywhat-gopro">
+		<h2>Go pro</h2>
+		<p><a href="http://plugins.leewillis.co.uk/downloads/say-what-pro/?utm_campaign=saywhatproupgrade">Say What? Pro</a> makes it even easier for you to change strings by offering <em><a href="http://plugins.leewillis.co.uk/doc_post/string-discovery/" target="_blank">String Discovery</a></em> and autocomplete, making it even easier to find the strings you need to change without diving through code.</p>
+		<p><strong>Upgrade to the Pro version today, and <em>save 20%</em> with the code <code>SAYWHATWP</code> at checkout.</strong></p>
+		<p><a href="http://plugins.leewillis.co.uk/downloads/say-what-pro/?utm_campaign=saywhatproupgrade" class="button button-primary">Go Pro now &raquo;</a></p>
+	</div>
 </div>
