0cbc6a81909bf45aad9d702891e33fb298a4b5b2
Lee Willis <leewillis77@gmail.com>
2016-04-11 17:15:14 +0100

    Support for _n() variants. Fixes #15

--- say-what-frontend.php
+++ say-what-frontend.php
@@ -25,31 +25,64 @@ class SayWhatFrontend {
 			$this->replacements[ $value['domain'] ][ $value['orig_string'] ][ $value['context'] ] = $value['replacement_string'];
 		}
 		add_filter( 'gettext', array( $this, 'gettext' ), 10, 3 );
+		add_filter( 'ngettext', array( $this, 'ngettext' ), 10, 5 );
 		add_filter( 'gettext_with_context', array( $this, 'gettext_with_context' ), 10, 4 );
+		add_filter( 'ngettext_with_context', array( $this, 'ngettext_with_context' ), 10, 6 );
 	}
 
 	/**
 	 * Perform a string replacement without context.
 	 */
 	public function gettext( $translated, $original, $domain ) {
-		return $this->gettext_with_context( $translated, $original, 'sw-default-context', $domain );
+		return $this->ngettext_with_context( $translated, $original, null, null, 'sw-default-context', $domain );
 	}
 
 	/**
 	 * Perform a string replacement with context.
-	 *
-	 * Plugins can use the say_what_domain_aliases filter to return an alias for their domain
-	 * if for any reason they change their text domain and want existing replacements to continue
-	 * working. The filter should return an array keyed on the current text domain with the value
-	 * set to an array of alternative domains to search for replacements. E.g
-	 *   $aliases['easy-digital-downloads'][] = 'edd';
-	 *   return $aliases;
 	 */
 	public function gettext_with_context( $translated, $original, $context, $domain ) {
+		return $this->ngettext_with_context( $translated, $original, null, null, $context, $domain );
+	}
+
+	/**
+	* Perform a (possibly) pluralised translation without context.
+	*/
+	public function ngettext( $translated, $single, $plural, $number, $domain ) {
+		return $this->ngettext_with_context( $translated, $single, $plural, $number, 'sw-default-context', $domain );
+	}
+
+    /**
+     * Perform a (possibly) pluralised translation with context.
+     *
+     * Note: This also handles the main logic for all other replacements.
+     *
+     * @param  string $translated The current string.
+     * @param  string $single     The original (singular) string.
+     * @param  string $plural     The original (pluralised) string.
+     *                            [May be NULL for non _n()-type calls]
+     * @param  int    $number     The number used to determine if singular or pluralised should be used.
+     *                            [May be NULL for non _n()-type calls]
+     * @param  [type] $context    The context, may be null for non _x()-type calls.
+     * @param  [type] $domain     The domain.
+     * @return [type]             The replaced string.
+     */
+    public function ngettext_with_context( $translated, $single, $plural, $number, $context, $domain ) {
+        /*
+         * Plugins can use the say_what_domain_aliases filter to return an alias for their domain
+         * if for any reason they change their text domain and want existing replacements to continue
+         * working. The filter should return an array keyed on the current text domain with the value
+         * set to an array of alternative domains to search for replacements. E.g
+         *   $aliases['easy-digital-downloads'][] = 'edd';
+         *   return $aliases;
+         */
 		static $domain_aliases = null;
 		if ( $domain_aliases === null ) {
 			$domain_aliases = apply_filters( 'say_what_domain_aliases', array() );
 		}
+        $original = $single;
+        if ( !is_null( $number ) && $number != 1 ) {
+                $original = $plural;
+        }
 		if ( isset( $this->replacements[ $domain ][ $original ][ $context ] ) ) {
 			return $this->replacements[ $domain ][ $original ][ $context ];
 		} elseif ( isset( $domain_aliases[ $domain ] ) ) {
