918c5769a2dcb38a574132a2ad70af9fb4c7045f
Lee Willis <lee@ademti-software.co.uk>
2020-03-10 11:15:21 +0000

    Open links in new window. Fixes #21

--- html/say-what-admin-addedit.php
+++ html/say-what-admin-addedit.php
@@ -3,7 +3,7 @@
 	<div id="icon-tools" class="icon32"></div>
 	<h2><?php _e( 'Text changes', 'say-what' ); ?></h2>
 
-	<p><?php _e( "Fill in the details of the original translatable string, the string's text domain, and the string you would like to use instead. For more information check out the <a href='https://plugins.leewillis.co.uk/doc_post/adding-string-replacement/'>getting started guide</a>." ); ?></p>
+	<p><?php _e( "Fill in the details of the original translatable string, the string's text domain, and the string you would like to use instead. For more information check out the <a href='https://plugins.leewillis.co.uk/doc_post/adding-string-replacement/' target='_blank' rel='noopener noreferrer'>getting started guide</a>." ); ?></p>
 	<form action="tools.php?page=say_what_admin&amp;say_what_action=addedit" method="post">
 		<input type="hidden" name="say_what_save" value="1">
 		<?php wp_nonce_field( 'swaddedit', 'nonce' ); ?>
@@ -15,11 +15,11 @@
 			<textarea class="say_what_orig_string" name="say_what_orig_string" rows="1" cols="120"><?php echo esc_textarea( $replacement->orig_string ); ?></textarea>
 		</p>
 		<p>
-			<label for="say_what_domain"><?php _e( 'Text domain', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/adding-string-replacement/"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
+			<label for="say_what_domain"><?php _e( 'Text domain', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/adding-string-replacement/" target="_blank" rel="noopener noreferrer"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
 			<input type="text" name="say_what_domain" size="30" value="<?php echo esc_attr( htmlspecialchars( $replacement->domain ) ); ?>"><br/>
 		</p>
 		<p>
-			<label for="say_what_context"><?php _e( 'Text context', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/replacing-wordpress-strings-context/"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
+			<label for="say_what_context"><?php _e( 'Text context', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/replacing-wordpress-strings-context/" target="_blank" rel="noopener noreferrer"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
 			<input type="text" name="say_what_context" size="30" value="<?php echo esc_attr( htmlspecialchars( $replacement->context ) ); ?>"><br/>
 		</p>
 		<p>
--- html/say-what-admin-list.php
+++ html/say-what-admin-list.php
@@ -13,37 +13,33 @@
                     <h2>Go pro</h2>
                     <hr>
                     <p>
-                        <a href="https://plugins.leewillis.co.uk/downloads/say-what-pro/?utm_source=wporg&amp;utm_medium=plugin&amp;utm_campaign=saywhatproupgrade">Say
-                            What? Pro</a> makes it even easier for you to change strings:
+                        <a href="https://plugins.leewillis.co.uk/downloads/say-what-pro/?utm_source=wporg&amp;utm_medium=plugin&amp;utm_campaign=saywhatproupgrade" target="_blank" rel="noopener noreferrer">Say What? Pro</a> makes it even easier for you to change strings:
                     <ul class="ul-disc">
                         <li>
                             <a href="https://plugins.leewillis.co.uk/doc_post/string-discovery/?utm_source=wporg&amp;utm_medium=plugin&amp;utm_campaign=saywhatproupgradesd"
-                               target="_blank">String Discovery</a> and autocomplete - find the strings you need without
+                               target="_blank" rel="noopener noreferrer">String Discovery</a> and autocomplete - find the strings you need without
                             diving through code.
                         </li>
                         <li>
                             <a href="https://plugins.leewillis.co.uk/doc_post/replace-words-across-whole-site/?utm_source=wporg&amp;utm_medium=plugin&amp;utm_campaign=saywhatproupgradesd"
-                               target="_blank">Wildcard string replacements</a> - replace individual words, or fragments
+                               target="_blank" rel="noopener noreferrer">Wildcard string replacements</a> - replace individual words, or fragments
                             across your whole site.
                         </li>
                         <li>
-                            <a href="https://plugins.leewillis.co.uk/doc_post/multi-lingual-string-replacements/?utm_source=wporg&amp;utm_medium=plugin&amp;utm_campaign=saywhatproupgrademl">Multi-lingual
-                                support</a> - set different replacements for different languages.</a></li>
+                            <a href="https://plugins.leewillis.co.uk/doc_post/multi-lingual-string-replacements/?utm_source=wporg&amp;utm_medium=plugin&amp;utm_campaign=saywhatproupgrademl" target="_blank" rel="noopener noreferrer">Multi-lingual support</a> - set different replacements for different languages.</a></li>
                         <li>Import &amp; export your strings between sites.</li>
                     </ul>
                     </p>
-                    <p><strong>Upgrade to the Pro version today, and <em>save 15%</em> with the code
-                            <code>WPSAYWHAT</code>
-                            at checkout.</strong></p>
+                    <p><strong>Upgrade to the Pro version today, and <em>save 15%</em> with the code <code>WPSAYWHAT</code> at checkout.</strong></p>
                     <p>
                     <center>
                         <a href="https://plugins.leewillis.co.uk/downloads/say-what-pro/?utm_source=wporg&amp;utm_medium=plugin&amp;utm_campaign=saywhatproupgradebtn"
-                           class="button button-primary">Go Pro now &raquo;</a></center>
+                           class="button button-primary" target="_blank" rel="noopener noreferrer">Go Pro now &raquo;</a></center>
                     </p>
                 </div>
             </div>
             <div class="treeware">
-                <a href="https://offset.earth/ademtisoftware?gift-trees" target="_blank" rel="noopener noreferrer">
+                <a href="https://offset.earth/ademtisoftware?gift-trees" target="_blank" rel="noopener noreferrer nofollow">
                     <img src="<?php echo esc_attr( plugins_url( 'say-what/img/treeware.svg' ) ); ?>" alt="Treeware">
                 </a>
                 <hr>
@@ -56,8 +52,8 @@
                         trees</a>.
                     If you support this package and contribute to the Treeware forest you’ll be creating employment for
                     local families and restoring wildlife habitats.</p>
-                <p>You can buy trees here <a target="_blank" rel="noopener noreferrer"
-                                             href="https://offset.earth/ademtisoftware?gift-trees" rel="nofollow">offset.earth/ademtisoftware</a>
+                <p>You can buy trees here <a target="_blank" rel="noopener noreferrer nofollow"
+                                             href="https://offset.earth/ademtisoftware?gift-trees">offset.earth/ademtisoftware</a>
                 </p>
             </div>
     </div>
