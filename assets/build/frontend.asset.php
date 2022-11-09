ce8a3ed36e02b9f166dd5d6723ed9d0878783295
Lee Willis <lee@ademti-software.co.uk>
2022-11-09 11:35:06 +0000

    JS updates

--- assets/build/frontend.asset.php
+++ assets/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks', 'wp-polyfill'), 'version' => '658db854f48fb86c32bd0f1d219c2d63');
+<?php return array('dependencies' => array('wp-hooks'), 'version' => 'f555924e251929bc28d2');
