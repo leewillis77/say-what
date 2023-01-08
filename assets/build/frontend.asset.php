99119293d50166405ab72ef082a930f4e77028b2
Lee Willis <lee@ademti-software.co.uk>
2023-01-08 09:01:13 +0000

    Package updates

--- assets/build/frontend.asset.php
+++ assets/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => 'f555924e251929bc28d2');
+<?php return array('dependencies' => array('wp-hooks'), 'version' => 'fd31684c45e4d85aeb4e');
