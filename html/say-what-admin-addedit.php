c97bd9ef0a7e94a4d5bb7d2396d539f2467295f0
Lee Willis <lee@ademti-software.co.uk>
2023-11-28 14:49:28 +0000

    Styling & screenshot updates

--- html/say-what-admin-addedit.php
+++ html/say-what-admin-addedit.php
@@ -12,25 +12,32 @@
 		<?php if ( ! empty ( $replacement->string_id ) ) : ?>
 			<input type="hidden" name="say_what_string_id" value="<?php echo esc_attr( htmlspecialchars( $replacement->string_id ) ); ?>">
 		<?php endif; ?>
-		<p>
-			<label for="say_what_orig_string"><?php esc_html_e( 'Original string', 'say-what' ); ?></label><br/>
+		<p class="saywhat_label_container">
+			<label for="say_what_orig_string"><?php esc_html_e( 'Original string', 'say-what' ); ?></label>
+        </p>
+        <p>
 			<textarea class="say_what_orig_string" name="say_what_orig_string" rows="1" cols="120"><?php echo esc_textarea( $replacement->orig_string ); ?></textarea>
 		</p>
-		<p>
-			<label for="say_what_domain"><?php esc_html_e( 'Text domain', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/adding-string-replacement/" target="_blank" rel="noopener noreferrer"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
+		<p class="saywhat_label_container">
+			<label for="say_what_domain"><?php esc_html_e( 'Text domain', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/adding-string-replacement/" target="_blank" rel="noopener noreferrer"><i class="dashicons dashicons-info">&nbsp;</i></a>
+        </p>
+        <p>
 			<input type="text" name="say_what_domain" size="30" value="<?php echo esc_attr( htmlspecialchars( $replacement->domain ) ); ?>"><br/>
 		</p>
-		<p>
-			<label for="say_what_context"><?php esc_html_e( 'Text context', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/replacing-wordpress-strings-context/" target="_blank" rel="noopener noreferrer"><i class="dashicons dashicons-info">&nbsp;</i></a><br/>
+		<p class="saywhat_label_container">
+			<label for="say_what_context"><?php esc_html_e( 'Text context', 'say-what' ); ?></label> <a href="https://plugins.leewillis.co.uk/doc_post/replacing-wordpress-strings-context/" target="_blank" rel="noopener noreferrer"><i class="dashicons dashicons-info">&nbsp;</i></a>
+        </p>
+        <p>
 			<input type="text" name="say_what_context" size="30" value="<?php echo esc_attr( htmlspecialchars( $replacement->context ) ); ?>"><br/>
 		</p>
-		<p>
-			<label for="say_what_replacement_string"><?php esc_html_e( 'Replacement string', 'say-what' ); ?></label><br/>
+		<p class="saywhat_label_container">
+			<label for="say_what_replacement_string"><?php esc_html_e( 'Replacement string', 'say-what' ); ?></label>
+        </p>
+        <p>
 			<textarea class="say_what_replacement_string" name="say_what_replacement_string" cols="120" rows="1"><?php echo esc_textarea( $replacement->replacement_string ); ?></textarea>
 		</p>
-		<p>
+		<p class="saywhat_label_container">
 			<input type="submit" class="button-primary" value="<?php  ! empty( $replacement->string_id ) ? _e( 'Update', 'say-what' ) : _e( 'Add', 'say-what' ); ?>">
 		</p>
 	</form>
-
 </div>
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: string, change, translation
 Requires at least: 6.2
 Requires PHP: 5.6
 Tested up to: 6.4
-Stable tag: 2.1.5
+Stable tag: 2.1.6
 
 == Description ==
 An easy-to-use plugin that allows you to change translatable strings from plugins / themes and WordPress core without editing code. Simply enter the current string, and what you want to replace it with and the plugin will automatically do the rest!
@@ -58,12 +58,14 @@ Not in the free plugin, however this is available in the [Pro version of the plu
 
 == Screenshots ==
 
-1. Finding a string to replace
-2. The admin screen - setting up a replacement
-3. The result
+1. List of configured string replacements
+2. Adding/editing a replacement
 
 
 == Changelog ==
+
+= 2.1.6 =
+* Change: Styling & documentation updates.
 
 = 2.1.5 = 
 * Fix: Avoid warning when sorting list table.
