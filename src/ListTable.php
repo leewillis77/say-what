f98fa81151949ae4e08eff536cda6bf128710b2d
Lee Willis <lee@ademti-software.co.uk>
2024-03-08 14:10:24 +0000

    CS

--- src/ListTable.php
+++ src/ListTable.php
@@ -13,8 +13,8 @@ class ListTable extends WP_List_Table {
 	/**
 	 * Description shown when no replacements configured
 	 */
-	function no_items() {
-		_e( 'No string replacements configured yet.', 'say-what' );
+	public function no_items() {
+		esc_html_e( 'No string replacements configured yet.', 'say-what' );
 	}
 
 	/**
@@ -23,17 +23,16 @@ class ListTable extends WP_List_Table {
 	 *
 	 * @throws Exception
 	 */
-	function get_columns() {
-		$columns = array(
-			/*'cb'        => 'Checkboxes',*/
+	public function get_columns() {
+		$columns = [
 			'string_id'          => 'String replacement ID (Internal)',
 			'orig_string'        => __( 'Original string', 'say-what' ),
 			'domain'             => __( 'Text domain', 'say-what' ),
 			'context'            => __( 'Text context', 'say-what' ),
 			'replacement_string' => __( 'Replacement string', 'say-what' ),
-			'edit_links'         => _x( '', 'Header for edit links on admin list table', 'say-what' ),
-			'delete_links'       => _x( '', 'Header for delete links on admin list table', 'say-what' ),
-		);
+			'edit_links'         => '',
+			'delete_links'       => '',
+		];
 
 		return $columns;
 	}
@@ -41,14 +40,14 @@ class ListTable extends WP_List_Table {
 	/**
 	 * Retrieve the items for display
 	 */
-	function prepare_items() {
+	public function prepare_items() {
 
 		global $wpdb, $table_prefix;
 
 		$columns               = $this->get_columns();
-		$hidden                = array( 'string_id' );
+		$hidden                = [ 'string_id' ];
 		$sortable              = $this->get_sortable_columns();
-		$this->_column_headers = array( $columns, $hidden, $sortable );
+		$this->_column_headers = [ $columns, $hidden, $sortable ];
 
 		// We don't use the replacements from the settings object, we query them separately to make
 		// ordering/pagination easier. This may turn out bad if people have "lots"
@@ -58,7 +57,7 @@ class ListTable extends WP_List_Table {
 		// Handle ordering of the results.
 		// The isset() check below validates that the passed data is a valid column name, and the value is
 		// not escaped in the query accordingly.
-		if ( isset ( $_GET['orderby'] ) && isset( $sortable[ $_GET['orderby'] ] ) ) {
+		if ( isset( $_GET['orderby'] ) && isset( $sortable[ $_GET['orderby'] ] ) ) {
 			$sql .= ' ORDER BY ' . $_GET['orderby'];
 			if ( isset( $_GET['order'] ) && strtolower( $_GET['order'] ) === 'desc' ) {
 				$sql .= ' DESC';
@@ -77,13 +76,13 @@ class ListTable extends WP_List_Table {
 	 * Indicate which columns are sortable
 	 * @return array A list of the columns that are sortable.
 	 */
-	function get_sortable_columns() {
-		return array(
-			'orig_string'        => array( 'orig_string', true ),
-			'domain'             => array( 'domain', false ),
-			'context'            => array( 'context', false ),
-			'replacement_string' => array( 'replacement_string', false )
-		);
+	public function get_sortable_columns() {
+		return [
+			'orig_string'        => [ 'orig_string', true ],
+			'domain'             => [ 'domain', false ],
+			'context'            => [ 'context', false ],
+			'replacement_string' => [ 'replacement_string', false ],
+		];
 	}
 
 	/**
@@ -98,50 +97,49 @@ class ListTable extends WP_List_Table {
 	/**
 	 * Specify the bulk actions available. Not used currently
 	 */
-	function get_bulk_actions() {
+	public function get_bulk_actions() {
 		// @TODO - implement bulk actions
-		//$actions = array ( 'delete' => __( 'Delete', 'say-what' ) );
-		return array();
+		//$actions = [ 'delete' => __( 'Delete', 'say-what' ) ];
+		return [];
 	}
 
 	/**
 	 * Checkboxes for the rows. Not used while we don't have bulk actions
 	 */
-	function column_cb( $item ) {
+	protected function column_cb( $item ) {
 		return sprintf( '<input type="checkbox" name="string_id[]" value="%d" />', $item['string_id'] );
 	}
 
 	/**
 	 * Output column data
 	 */
-	function column_default( $item, $column_name ) {
+	protected function column_default( $item, $column_name ) {
 		return esc_html( htmlspecialchars( $item[ $column_name ] ) );
 	}
 
 	/**
 	 * Output an edit link for the row
 	 */
-	function column_edit_links( $item ) {
+	protected function column_edit_links( $item ) {
 		return '<a href="tools.php?page=say_what_admin&amp;say_what_action=addedit&amp;id=' .
-			   urlencode( $item['string_id'] ) .
-			   '&amp;nonce=' .
-			   urlencode( wp_create_nonce( 'swaddedit' ) ) .
-			   '">' .
-			   __( 'Edit', 'say-what' ) .
-			   '</a>';
+				rawurlencode( $item['string_id'] ) .
+				'&amp;nonce=' .
+				rawurlencode( wp_create_nonce( 'swaddedit' ) ) .
+				'">' .
+				__( 'Edit', 'say-what' ) .
+				'</a>';
 	}
 
 	/**
 	 * Output a delete link for the row
 	 */
-	function column_delete_links( $item ) {
+	protected function column_delete_links( $item ) {
 		return '<a href="tools.php?page=say_what_admin&say_what_action=delete&id=' .
-			   urlencode( $item['string_id'] ) .
-			   '&nonce=' .
-			   urlencode( wp_create_nonce( 'swdelete' ) ) .
-			   '">' .
-			   __( 'Delete', 'say-what' ) .
-			   '</a>';
+				rawurlencode( $item['string_id'] ) .
+				'&nonce=' .
+				rawurlencode( wp_create_nonce( 'swdelete' ) ) .
+				'">' .
+				__( 'Delete', 'say-what' ) .
+				'</a>';
 	}
-
 }
